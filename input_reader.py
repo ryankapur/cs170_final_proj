91ecb52e88fbd27193b5707e6b1833244dbfe88f
Ryan Kapur <ryankapur@berkeley.edu>
2016-04-29 19:31:33 -0700

    Pushing working version of Tarjan with minor dict error

--- input_reader.py
+++ input_reader.py
@@ -1,7 +1,7 @@
 #phase1-processed
-import pprint
+import pprint, tarjan
 
-f = open("phase1-processed/95.in", "r")
+f = open("phase1-processed/91.in", "r")
 numNodes = f.readline().split()
 numNodes = int(numNodes[0])
 print('numNodes ' + str(numNodes))	
@@ -26,21 +26,53 @@ print('numChild ', numChild)
 
 #create 2D array logic
 d = [[0 for j in range(numNodes)] for i in range(numNodes)]
+
+dct = {}
+
+
+
+#Creating dictionary representation to pass into tarjan library
 for i in xrange(numNodes):
 	line = f.readline().split()
-	
 	#checks each value is an int
 	for j in xrange(numNodes):
-		if not line[j].isdigit():
-			print "Line " + str(i+2) + " must contain numNodes integers."
-		d[i][j] = int(line[j])
-		# if d[i][j] < 0 or d[i][j] > 1:
-		# 	return "The adjacency matrix must be comprised of 0s and 1s."
+		print "line: ", 
+		print "line[j] is: ", line[j]
+		if int(line[j]) == 1:
+			print("got inside the if statement")
+			if i in dct:
+				print("about to modify index i: ", i)
+				current_edges = dct[i]
+				dct[i] = current_edges.append(j)
+			else:
+				print("about to create a new key, value for this index, ", i)
+				dct[i] = [j]
 
-# for i in xrange(numNodes):
-# 	if d[i][i] != 0:
-# 		return "A node cannot have an edge to itself."
+
 
 for i in range(0, len(d)):
 	for j in range(0,len(d[0])):
 		print "d at: ", i, " ", j, " ", d[i][j]
+
+print("dct is ", dct)
+
+result = tarjan.tarjan(dct)
+print 'result jajaja ', result
+
+
+# for i in xrange(numNodes):
+# 	line = f.readline().split()
+# 	#checks each value is an int
+# 	for j in xrange(numNodes):
+
+# 		if not line[j].isdigit():
+# 			print "Line " + str(i+2) + " must contain numNodes integers."
+# 		d[i][j] = int(line[j])
+# 		# if d[i][j] < 0 or d[i][j] > 1:
+# 		# 	return "The adjacency matrix must be comprised of 0s and 1s."
+
+# # for i in xrange(numNodes):
+# # 	if d[i][i] != 0:
+# # 		return "A node cannot have an edge to itself."
+
+
